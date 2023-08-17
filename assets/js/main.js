6061a17d1c189c0acc9a8bc1a1920f29aeda44f0
luisfe97 <luisfelipegomezr@hotmail.com>
2023-08-16 23:37:01 -0500

    init

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+var figure = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+    $('video', this).get(0).currentTime = 0;
+}
